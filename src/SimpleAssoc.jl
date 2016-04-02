51a083b9e0032858d381d29c0f5a05f4993d63fc
Erik Schnetter <schnetter@gmail.com>
2016-04-01 20:45:28 -0400

    Initial version

--- src/SimpleAssoc.jl
+++ src/SimpleAssoc.jl
@@ -1,5 +1,151 @@
 module SimpleAssoc
 
-# package code goes here
+immutable AssocTuple{N,K,T} <: Associative{K,T}
+    elems::NTuple{N, Pair{K,T}}
+end
 
-end # module
+################################################################################
+
+import Base:
+    copy,
+    delete!,
+    done,
+    eachindex,
+    eltype,
+    empty!,
+    get,
+    get!,
+    getkey,
+    haskey,
+    isempty,
+    keys,
+    length,
+    next,
+    pop!,
+    setindex!,
+    start,
+    values
+
+export AssocArray
+
+"""
+    type AssocArray{K,V}
+
+A simple associative collection based on (unsorted) arrays
+
+An `AssocArray` is a simple associative collection. Functionally it is
+equivalent to a `Dict`. The major difference is that an `AssocArray`
+uses a simpler internal representation, based on an unsorted array.
+This can be faster than a `Dict` if the collection is small, or if the
+collection is searched only infrequently.
+"""
+type AssocArray{K,V} <: Associative{K,V}
+    elems::Vector{Pair{K,V}}
+    AssocArray() = new(Vector{Pair{K,V}}())
+    function AssocArray(iter)
+        elems = Pair{K,V}[el[1]=>el[2] for el in iter]
+        new(elems)
+    end
+    function AssocArray(elems::Pair...)
+        elems = Pair{K,V}[el for el in elems]
+        new(elems)
+    end
+    function AssocArray(a::AssocArray)
+        new(copy(a.elems))
+    end
+end
+
+function AssocArray(iter)
+    K = fieldtype(eltype(iter),1)
+    V = fieldtype(eltype(iter),2)
+    AssocArray{K,V}(iter)
+end
+AssocArray(a::AssocArray) = AssocArray{keytype(a), valtype(a)}(a)
+AssocArray(elems::Pair...) = AssocArray(elems)
+
+copy(a::AssocArray) = AssocArray(a)
+
+eltype{K,V}(::AssocArray{K,V}) = Pair{K,V}
+
+isempty(a::AssocArray) = isempty(a.elems)
+length(a::AssocArray) = length(a.elems)
+
+start(a::AssocArray) = start(a.elems)
+done(a::AssocArray, st) = done(a.elems, st)
+next(a::AssocArray, st) = next(a.elems, st)
+
+immutable AssocArrayIter{I,K,V}
+    arr::AssocArray{K,V}
+end
+start(ai::AssocArrayIter) = start(ai.arr.elems)
+done(ai::AssocArrayIter, st) = done(ai.arr.elems, st)
+function next{I}(ai::AssocArrayIter{I}, st)
+    it, st = next(ai.arr.elems, st)
+    it[I], st
+end
+keys{K,V}(a::AssocArray{K,V}) = AssocArrayIter{1,K,V}(a)
+values{K,V}(a::AssocArray{K,V}) = AssocArrayIter{2,K,V}(a)
+eachindex(a::AssocArray) = keys(a)
+
+empty!(a::AssocArray) = empty!(a.elems)
+
+function pop!(a::AssocArray)
+    if isempty(a)
+        throw(BoundsError(a))
+    end
+    pop!(a.elems)
+end
+
+function getkey(a::AssocArray, k, d)
+    for el in a.elems
+        if el.first === k
+            return el.first
+        end
+    end
+    d
+end
+
+function get(a::AssocArray, k, d)
+    for el in a.elems
+        if el.first === k
+            return el.second
+        end
+    end
+    d
+end
+
+function get!(a::AssocArray, k, d)
+    for el in a.elems
+        if el.first === k
+            return el.second
+        end
+    end
+    a[k] = d
+end
+
+function setindex!(a::AssocArray, v, k)
+    for i in eachindex(a.elems)
+        if a.elems[i].first === k
+            a.elems[i] = k => v
+            return v
+        end
+    end
+    push!(a.elems, k => v)
+    v
+end
+
+function delete!(a::AssocArray, k)
+    for i in eachindex(a.elems)
+        if a.elems[i].first === k
+            if i < length(a.elems)
+                a.elems[i] = pop!(a.elems)
+            else
+                pop!(a.elems)
+            end
+            return a
+        end
+    end
+    a
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,133 @@
 using SimpleAssoc
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Constructors
+a0 = AssocArray{Char,Int}()
+a1 = AssocArray{Char,Int}('a'=>1)
+a2 = AssocArray{Char,Int}('a'=>1, 'b'=>2)
+
+# Deducing the element type
+a1 = AssocArray('a'=>1)
+@test isa(a1, AssocArray{Char,Int})
+a2 = AssocArray('a'=>1, 'b'=>2)
+@test isa(a2, AssocArray{Char,Int})
+
+# Constructing from a Pair vs. constructing from an iterator
+a1 = AssocArray(('a'=>1) => ('b'=>2))
+@test isa(a1, AssocArray{Pair{Char,Int}, Pair{Char,Int}})
+a2 = AssocArray(('a'=>1), ('b'=>2))
+@test isa(a2, AssocArray{Char,Int})
+a3 = AssocArray((('a'=>1), ('b'=>2)))
+@test isa(a3, AssocArray{Char,Int})
+a4 = AssocArray(Pair{Char,Int}[('a'=>1), ('b'=>2)])
+@test isa(a4, AssocArray{Char,Int})
+a5 = AssocArray((('a',1), ('b',2)))
+@test isa(a5, AssocArray{Char,Int})
+a6 = AssocArray([('a',1), ('b',2)])
+@test isa(a6, AssocArray{Char,Int})
+
+# Type properties
+@test eltype(a0) === Pair{Char,Int}
+@test eltype(a1) === Pair{Pair{Char,Int}, Pair{Char,Int}}
+@test eltype(a2) === Pair{Char,Int}
+
+# Object properties
+@test isempty(a0)
+@test !isempty(a1)
+@test !isempty(a2)
+@test length(a0) == 0
+@test length(a1) == 1
+@test length(a2) == 2
+
+# Iterators
+"Compare two iterables without regard to order"
+function itercmp(i1, i2)
+    c1, c2 = collect(i1), collect(i2)
+    length(c1) == length(c2) && isequal(Set(c1), Set(c2))
+end
+
+@test itercmp(a0, Pair{Char,Int}[])
+@test itercmp(a1, Pair{Pair{Char,Int}, Pair{Char,Int}}[('a'=>1) => ('b'=>2)])
+@test itercmp(a2, Pair{Char,Int}[('a'=>1), ('b'=>2)])
+
+@test itercmp(keys(a0), Char[])
+@test itercmp(keys(a1), Pair{Char,Int}['a'=>1])
+@test itercmp(keys(a2), Char['a', 'b'])
+
+@test itercmp(values(a0), Int[])
+@test itercmp(values(a1), Pair{Char,Int}['b'=>2])
+@test itercmp(values(a2), Int[1, 2])
+
+@test itercmp(eachindex(a0), keys(a0))
+@test itercmp(eachindex(a1), keys(a1))
+@test itercmp(eachindex(a2), keys(a2))
+
+# Mutating operations
+empty!(a0)
+@test isempty(a0)
+empty!(a1)
+@test isempty(a1)
+empty!(a2)
+@test isempty(a2)
+
+# Element-wise access
+
+a0['a'] = 1
+@test a0['a'] == 1
+@test length(a0) == 1
+a0['b'] = -1
+@test a0['b'] == -1
+@test length(a0) == 2
+a0['b'] = 2
+@test a0['b'] == 2
+@test length(a0) == 2
+get!(a0, 'b', 3)
+@test a0['b'] == 2
+@test length(a0) == 2
+get!(a0, 'c', 3)
+@test a0['c'] == 3
+@test length(a0) == 3
+@test get(a0, 'c', 4) == 3
+@test get(a0, 'd', 4) == 4
+@test haskey(a0, 'c')
+@test getkey(a0, 'c', 4) == 'c'
+@test !haskey(a0, 'd')
+@test getkey(a0, 'd', 4) == 4
+@test itercmp(a0, ('a'=>1, 'b'=>2, 'c'=>3))
+
+@test_throws KeyError a0['A']
+
+# Delete elements
+
+delete!(a0, 'b')
+@test length(a0) == 2
+@test_throws KeyError a0['b']
+delete!(a0, 'c')
+@test length(a0) == 1
+@test_throws KeyError a0['c']
+delete!(a0, 'a')
+@test length(a0) == 0
+@test_throws KeyError a0['a']
+delete!(a0, 'a')
+@test length(a0) == 0
+
+# Copy collection
+
+a4c = copy(a4)
+@test isequal(a4c, a4)
+@test a4c !== a4
+
+# push and pop
+
+x = pop!(a4)
+@test length(a4) == 1
+y = pop!(a4)
+@test length(a4) == 0
+@test x != y
+
+push!(a4, x)
+@test length(a4) == 1
+push!(a4, y)
+@test length(a4) == 2
+
+@test isequal(a4, a4c)
